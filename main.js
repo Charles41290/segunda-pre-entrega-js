338c6ed83ae90f343f7dcf6f0940cc16c854cb08
Romero Rodriguez <cromero@se.veng.com.ar>
2023-11-23 22:01:37 -0300

    creando el repo

---
+++ main.js
@@ -0,0 +1,14 @@
+/*
+El presente codigo corresponde a un proyecto de una libreria la cual posee una pagina web 
+en la cual se pueden realizar compras. Hasta el momento se han creado un inventario de 7
+libros y 5 autores. Se puede realizar una busqueda de los libros disponibles por titulo
+o autor de este.
+
+Los titulos son:
+El señor de los anillos, Cien años de soledad, El cementerio de Praga, Travesura de la niña mala,
+El túnel, El nombre de la Rosa, Del amor y otros demonios.
+
+En autores se tiene:
+J.R.R.Tolkien, Gabriel Garcia Marquez, Umberto Eco, Ernesto Sabato y Mario Vargas Llosa
+*/
+
